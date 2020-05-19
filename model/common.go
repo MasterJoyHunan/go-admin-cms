8cd30c693ec6b38332b10ea9a3a2a59a6d81a2b5
Master Joy <386442876@qq.com>
2020-05-19 09:30:24 +0800

    重新对权限 ID 进行规划

--- model/common.go
+++ model/common.go
@@ -41,6 +41,16 @@ func Setup() {
 	}
 }
 
+
+// 通用分页获取偏移量
+func GetOffset(page, pageSize int) int {
+	if page <= 1 {
+		return 0
+	}
+	return (page - 1) * pageSize
+}
+
+
 // 设置条件
 func MultiWhere(where ...interface{}) func(*gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
@@ -75,8 +85,8 @@ func InitSql() {
 	// 初始化
 	Db.Exec("insert into admin_user (id, user_name, password, real_name, tel, status) values (1, 'admin', '$2a$10$057uuCLoKja2J04GLuWl1eNnwQtS7HxvookpbBa0thTHq7/fIaNF6', 'joy', '13054174174', 1)")
 
-	Db.Exec("insert into role (id, pid, name, auth) values (1, 0, '超级管理员', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18')")
-	Db.Exec("insert into role (id, pid, name, auth) values (2, 1, '系统维护管理员', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18')")
+	Db.Exec("insert into role (id, pid, name, auth) values (1, 0, '超级管理员', '10,11,1100,1101,110000,110001,110002,110003,110004,110005,110100,110101,110102,110103,110104,110105')")
+	Db.Exec("insert into role (id, pid, name, auth) values (2, 1, '系统维护管理员', '10,11,1100,1101,110000,110001,110002,110003,110004,110005,110100,110101,110102,110103,110104,110105')")
 
 	Db.Exec("insert into casbin_rule (p_type, v0, v1) values ('g', 'user:1', 'role:1')")
 	Db.Exec("insert into casbin_rule (p_type, v0, v1, v2) values ('p', 'role:1', '/role', 'get')")
@@ -92,30 +102,22 @@ func InitSql() {
 	Db.Exec("insert into casbin_rule (p_type, v0, v1, v2) values ('p', 'role:1', '/auth/role', 'get')")
 	Db.Exec("insert into casbin_rule (p_type, v0, v1, v2) values ('p', 'role:1', '/auth/tree', 'get')")
 
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (1, 0, '首页', 1, '', '', 'index')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (2, 0, '后台管理', 1, '', '', 'admin')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (3, 0, '项目管理', 1, '', '', 'project')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (4, 0, '文章管理', 1, '', '', 'article')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (5, 2, '角色', 1, '', '', 'admin-role')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (6, 2, '用户', 1, '', '', 'admin-user')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (7, 5, '角色列表', 0, '/role', 'get', 'admin-role-list')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (8, 5, '角色详情', 0, '/role/:id', 'get', 'admin-role-detail')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (9, 5, '角色添加', 0, '/role', 'post', 'admin-role-add')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (10, 5, '角色修改', 0, '/role/:id', 'put', 'admin-role-edit')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (11, 5, '角色删除', 0, '/role/:id', 'delete', 'admin-role-del')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (12, 6, '用户列表', 0, '/adminUser', 'get', 'admin-user-list')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (13, 6, '用户详情', 0, '/adminUser/:id', 'get', 'admin-user-detail')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (14, 6, '用户添加', 0, '/adminUser', 'post', 'admin-user-add')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (15, 6, '用户修改', 0, '/adminUser/:id', 'put', 'admin-user-edit')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (16, 6, '用户删除', 0, '/adminUser/:id', 'delete', 'admin-user-del')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (17, 6, '获取角色树', 0, '/auth/role', 'get', 'admin-user-role-tree')")
-	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (18, 5, '获取权限树', 0, '/auth/tree', 'get', 'admin-user-auth-tree')")
-}
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (10, 0, '首页', 1, '', '', 'index')")
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (11, 0, '后台管理', 1, '', '', 'admin')")
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (1100, 11, '角色', 1, '', '', 'admin-role')")
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (1101, 11, '用户', 1, '', '', 'admin-user')")
 
-// 通用分页获取偏移量
-func GetOffset(page, pageSize int) int {
-	if page <= 1 {
-		return 0
-	}
-	return (page - 1) * pageSize
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (110000, 1100, '获取权限树', 0, '/auth/tree', 'get', 'admin-user-auth-tree')")
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (110001, 1100, '角色列表', 0, '/role', 'get', 'admin-role-list')")
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (110002, 1100, '角色详情', 0, '/role/:id', 'get', 'admin-role-detail')")
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (110003, 1100, '角色添加', 0, '/role', 'post', 'admin-role-add')")
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (110004, 1100, '角色修改', 0, '/role/:id', 'put', 'admin-role-edit')")
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (110005, 1100, '角色删除', 0, '/role/:id', 'delete', 'admin-role-del')")
+
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (110100, 1101, '获取角色树', 0, '/auth/role', 'get', 'admin-user-role-tree')")
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (110101, 1101, '用户列表', 0, '/adminUser', 'get', 'admin-user-list')")
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (110102, 1101, '用户详情', 0, '/adminUser/:id', 'get', 'admin-user-detail')")
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (110103, 1101, '用户添加', 0, '/adminUser', 'post', 'admin-user-add')")
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (110104, 1101, '用户修改', 0, '/adminUser/:id', 'put', 'admin-user-edit')")
+	Db.Exec("insert into auth (id, pid, name, is_menu, api, action, ext) values (110105, 1101, '用户删除', 0, '/adminUser/:id', 'delete', 'admin-user-del')")
 }
